0862712fe359bb6ce8aed91638c4f0c31e34f8fb
Krystian Owczarek <krystian11223@onet.pl>
2022-09-16 18:34:18 +0200

    Trzeci commit - modyfikacja strony

--- flex-galeria/galeria.js
+++ flex-galeria/galeria.js
@@ -1,64 +1,125 @@
 function start()
 {
-    const imgs = document.querySelectorAll("img");
+    const imgs = document.querySelectorAll(".column img");
     const bigPicture = document.querySelector(".bigPicture");
-    const button = document.querySelector("button");
+    const exit = document.querySelector(".exit");
     const body = document.querySelector("body");
+    const next = document.querySelector(".next");
+    const back = document.querySelector(".back");
+    const bigPhoto = document.querySelector(".bigPhoto");
 
+    //zmienna globalna
+    let currentIndex;
+    let windowWidth = window.innerWidth;
+
+    setTimeout('start()',100);
     //pętla dzięki której funkcje są wykonywane dla każdego zdjęcia z osobna
-    for(const img of imgs)
+    if(windowWidth<=600)
     {
-        const value = img.getAttribute("data-id");
-
-        //funkcja dodająca możliwość podglądu po naciśnięciu na zdjęcie
-        img.addEventListener("click",function(){
-                podglądZdjęć();
-        })
-
-        //funkcja uruchamiająca podgląd zdjęć
-        function podglądZdjęć()
-        {
-            bigPicture.style.cssText = `
-                display: flex; 
-	            justify-content: center; 
-	            align-items: center;
-                z-index: 1;
-                position: fixed;
-            `;
-
-            button.style.cssText = `
-                display: flex;
-                z-index: 1;
-            `;
-
-            body.style.cssText = `
-                margin: 0;
-                padding: 0;
-                overflow: hidden;
-            `;
-
-            bigPicture.innerHTML = `<img src="img/${value}.jpg" style="cursor: auto; transform: scale(1,1); width: 60%; height: auto;"/>`;
-        }
+        //alert('nothing');
+        imgs.forEach((img, index) => {
+            //funkcja dodająca możliwość podglądu po naciśnięciu na zdjęcie
+            img.addEventListener("click",(e) => {
+                bigPicture.classList.remove('bigPicture');
+            });
+        });
+    }else{
+        imgs.forEach((img, index) => {
+            //funkcja dodająca możliwość podglądu po naciśnięciu na zdjęcie
+            img.addEventListener("click",(e) => {
+                previewPhoto();
+                bigPhoto.src = e.target.src;
+                closePreview();
+                //do zmiennej globalnej przypisujemy index naciśniętego zdjęcia
+                currentIndex = index;
+            });
+        });
     }
 
-    //przycisk zamykający podgląd
-    button.addEventListener('click', function(){
-        
-        body.style.cssText = `
-        overflow: visible;
-        `;
-
-        bigPicture.style.cssText = `
-            display: none; 
-            justify-content: center; 
-            align-items: center;
-            z-index: -1;
-        `;
-
-        button.style.cssText = `
-                display: none;
-            `;
+    next.addEventListener('click',() => {
+        //jeśli index jest równy indexowi ostatniego obrazka
+        if(currentIndex==27){
+            //to zmień index na -1
+            currentIndex = -1;
+            //i dodaj do niego 1 co daje 0 czyli startujemy od pierwszego obrazka
+            currentIndex++;
+            //zmień źródło wyświetlanego obrazka na źródło obrazka odpowiadające danemu indexowi
+            bigPhoto.src = imgs[currentIndex].src;
+        } 
+        //w przeciwnym razie tylko zmień index obrazka i podmień src
+        else
+        {
+            currentIndex++;
+            bigPhoto.src = imgs[currentIndex].src;
+        } 
     });
+    
+    back.addEventListener('click',() => {
+        if(currentIndex==0){
+            currentIndex = 28;
+            currentIndex--;
+            bigPhoto.src = imgs[currentIndex].src;
+        } else
+        {
+            currentIndex--;
+            bigPhoto.src = imgs[currentIndex].src;
+        } 
+    });
+
+    //funkcja uruchamiająca podgląd zdjęć
+    function previewPhoto()
+    {
+        bigPicture.style.cssText = `
+            display: flex; 
+	        justify-content: center; 
+	        align-items: center;
+            z-index: 1;
+            position: fixed;
+        `;
+
+        next.style.cssText = `
+            display: flex;
+            z-index: 1;
+        `;
+
+        back.style.cssText = `
+            display: flex;
+            z-index: 1;
+        `;
+
+        body.style.cssText = `
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        `;
+    }
+
+    //funkcja zamykająca podgląd
+    function closePreview()
+    {
+        exit.style.cssText = `
+            display: flex;
+            z-index: 1;
+        `;
+
+        exit.addEventListener('click',() => {
+            
+            body.style.cssText = ` overflow: visible; `;
+
+            bigPicture.style.cssText = `
+                display: none; 
+                justify-content: center; 
+                align-items: center;
+                z-index: -1;
+            `;
+
+            exit.style.cssText = ` display: none; `;
+
+            next.style.cssText = ` display: none; `;
+
+            back.style.cssText = ` display: none; `;
+        });
+    }
 }
 
 window.onload = start;
